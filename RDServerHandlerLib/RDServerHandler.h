79842f665b4b6ad8568860d14dc14b7e27f00352
Ravi Deshmukh <ravi.deshmukh@nttdata.com>
2015-01-08 15:46:02 +0530

    Lib Ver-4.0 - added a support for adding multiple headers in request

--- RDServerHandlerLib/RDServerHandler.h
+++ RDServerHandlerLib/RDServerHandler.h
@@ -1,4 +1,4 @@
-//  RDServerHandlerBlock.h
+//  RDServerHandler.h
 //  Created by Ravi Deshmukh
 //  Copyright (c) 2014 Ravi Deshmukh. All rights reserved.
 
@@ -45,5 +45,18 @@ typedef void (^RDServerHandlerBlock)(NSString * ,NSDictionary *, NSError *);
  */
 
 - (void)callServiceWithURL:(NSURL *)url contentType:(NSString *)strContentType andBodyString:(NSString *)strBody withMethod:(NSString *)methodType isSynchronous:(BOOL)isSynchronous authorizationHeader:(NSString *)authorizationHeader ;
+
+/**
+ *  allows to call web service using synchronous and asynchronous way. This method resposnds to success and failure callbacks which will be intimating sending controller after every resposne received. Pass username and password if authentication needs to be passed for any specific service.
+ *
+ *  @param url               URL for API to be called
+ *  @param strContentType    Content-Type
+ *  @param strBody           body string in case any .(depending on type of web service call)
+ *  @param methodType        Type of Method - GET, POST, PUT etc (In object forms like NSDictionary/NSArray).
+ *  @param isSynchronous     is Request Synchronous/ Asynchronous.
+ *  @param headersDictionary It will be dictionary with Key Value pairs for all header to be passed with the request
+ */
+
+- (void)callServiceWithURL:(NSURL *)url contentType:(NSString *)strContentType andBodyString:(NSString *)strBody withMethod:(NSString *)methodType isSynchronous:(BOOL)isSynchronous withHeaders:(NSDictionary *)headersDictionary ;
 
 @end
